e2a6fba228a97bca78ea4cb1adaf12dc4adec21e
david musembi <dmusembi702@gmail.com>
2025-06-10 23:33:46 +0300

    i love coding

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -3,5 +3,5 @@ const myVar = "JavaScript is amazing";
 //console.log(myVar);
 
 
-const myVal = "C is fun \n" + " Python is cool \n" + "Javscript is amazing \n"
+const myVal = "C is fun \n" + "Python is cool \n" + "Javascript is amazing \n"
 console.log(myVal)
