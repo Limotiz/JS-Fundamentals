c66603f5530379e62d9a1d11e8a45577f19a6c7b
david musembi <dmusembi702@gmail.com>
2025-06-13 15:04:26 +0300

    musembi fire

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,18 +1,32 @@
 const arg = process.argv[2];
 
-const num = Number('ALX');
+
+
+const num = Number('89');
+
+
+
+
 
 
 
 if (arg !== undefined && Number.isInteger(num)) {
 
+
+
   console.log(`My number: ${num}`);
+
+
 
 } else {
 
+
+
   console.log('Not a number');
 
-} 
+
+
+}
 
 
 
