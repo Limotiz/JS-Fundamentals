df730648423fe8f5ca0f34abefe666c3f48da1db
david musembi <dmusembi702@gmail.com>
2025-06-13 14:13:55 +0300

    yes i can

---
+++ 5-to_integer.js
@@ -0,0 +1,20 @@
+const arg = process.argv[2];
+
+const num = Number(89);
+
+
+
+if (arg !== undefined && Number.isInteger(num)) {
+
+  console.log(`My number: ${num}`);
+
+} else {
+
+  console.log('Not a number');
+
+} 
+
+
+
+
+
